256f9d75453a80f76c1e68674163d86c465df92e
Geri <admin@geri.dev>
2023-03-25 20:30:49 +0100

    feat: emergency summary start

--- butler.js
+++ butler.js
@@ -15,29 +15,53 @@ const openai = new OpenAIApi(configuration);
 const app = express();
 
 // Let any other request time out, we don't care
-app.get('/*', () => {
-});
 app.put('/*', () => {
 });
 app.delete('/*', () => {
 });
 
-app.post('/fillSummary/:cardId', authenticate, async (req, res) => {
+app.get('/emergencySummary/:cardId', /* authenticate, */ async (req, res) => {
+    const cardId = req.params.cardId;
+    console.info(`Handling emergency summary for card ${cardId}`);
+
+    try {
+        const {card, formattedComments} = await getCardDetails(cardId);
+        const text = await getPromptAnswer(`
+        Write up a Trello card comment with a timeline of events that took place in the card.
+        The format of each timeline entry should be: - \`YYYY/MM/DD DATE AND HH:MM TIME HERE\`: BRIEF DESCRIPTION HERE
+        The date and time should be copied from the comment.
+        Surround the time and date with backticks, such as \`DATE AND TIME HERE\`, this is crucial.
+        Almost each comment should have its own entry. The description for each should be 1-2 sentences at most.
+        
+        You will now get the title, description and each comment on the card.
+        Comments start with the time in '[]', then the name of the user and the message after ':'
+        --- 
+        Title: ${card.name}
+        ---
+        Description: ${card.desc}
+        ---
+        Comments:
+        ${formattedComments}
+        ---
+        Data over, now write your timeline of the card:
+        `, false);
+
+        console.info(text);
+
+        res.json({text});
+    } catch (e) {
+        res.status(500).json({result: e.response.data.error || e.message || e});
+    }
+});
+
+app.post('/bugsSummary/:cardId', authenticate, async (req, res) => {
     const cardId = req.params.cardId;
     console.info(`Handling summary for card ${cardId}`);
 
     try {
+        const {card, formattedComments} = await getCardDetails(cardId);
 
-        const card = await getCardById(cardId);
-        const comments = await getCardComments(cardId);
-
-        let formattedComments = '';
-        for (const comment of comments) {
-            const creator = await getMember(comment.idMemberCreator);
-            formattedComments += `[${comment.date}] ${creator.fullName}: ${comment?.data?.text?.replace('\n', ' ')}\n`;
-        }
-
-        const prompt = dedent(`
+        const text = await getPromptAnswer(`
         Summarize only the issue in this Trello card as a less than a 20-word meeting writeup without new lines.
         You will now get the title, description and each comment on the card.
         Comments start with the time in '[]', then the name of the user and the message after ':'
@@ -46,20 +70,12 @@ app.post('/fillSummary/:cardId', authenticate, async (req, res) => {
         ---
         Description: ${card.desc}
         ---
-        Comments: ${formattedComments}
+        Comments:
+        ${formattedComments}
         ---
         Data over, now write your summary of the card:
         `);
 
-        const completion = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: prompt,
-            temperature: 0.6,
-            max_tokens: 2048
-        });
-
-        let text = completion.data.choices[0].text || '';
-        text = text.replaceAll('\n', ' ').trim();
 
         const response = await updateCustomField(cardId, text);
         console.log(text);
@@ -77,7 +93,7 @@ async function getCardById(cardId) {
 }
 
 async function getCardComments(cardId) {
-    const resp = await axios.get(`${formatEndpoint(`/cards/${cardId}/actions`)}&filter=commentCard`);
+    const resp = await axios.get(`${formatEndpoint(`/cards/${cardId}/actions`)}&filter=commentCard,copyCommentCard`);
     if (resp.status !== 200) throw new Error(`Trello API returned code ${resp.status}: ${resp.message}`);
     return resp.data;
 }
@@ -105,6 +121,60 @@ function authenticate(req, res, next) {
     } else {
         return res.status(401).json({error: 'Invalid token'});
     }
+}
+
+async function getPromptAnswer(prompt, format = true) {
+    prompt = dedent(prompt);
+    const completion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: prompt,
+        temperature: 0.6,
+        max_tokens: 2048
+    });
+
+    let text = completion.data.choices[0].text || '';
+    if (!format) return text;
+    return text.replaceAll('\n', ' ').trim();
+}
+
+async function getCardDetails(cardId) {
+    const card = await getCardById(cardId);
+    const comments = await getCardComments(cardId);
+
+    let formattedComments = '';
+    for (const comment of comments) {
+        const creator = await getMember(comment.idMemberCreator);
+
+        const estFormatter = new Intl.DateTimeFormat('hu-HU', {
+            timeZone: 'EST',
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false,
+            formatMatcher: 'basic',
+            separator: '/'
+        });
+
+        // Format the date as a string in yyyy/mm/dd HH:MM format but for EST
+        const regex = /^(?<year>\d{4})\. (?<month>\d{2})\. (?<day>\d{2})\. (?<hour>\d{2}):(?<minute>\d{2})$/;
+        const rawDate = estFormatter.format(new Date(comment.date));
+        const match = regex.exec(rawDate);
+
+        let date = 'Unknown';
+        if (match) {
+            const {year, month, day, hour, minute} = match.groups;
+            date = `${year}/${month}/${day} ${hour}:${minute}`;
+        }
+
+        let text = comment?.data?.text?.replace('\n', ' ');
+        if (text.length > 512) text = text.substring(0, 512);
+
+        formattedComments += `[${date}] ${creator.fullName}: ${text}\n`;
+    }
+
+    return {card, formattedComments};
 }
 
 app.listen(process.env.port || 12345, () => {
